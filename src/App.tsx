c09c8c08d06411d0fc4e056d4216f8518f7cfba7
diegoHeartless <mnnvh@yandex.ru>
2025-11-16 16:58:19 +0500

    Refactor code for consistency by standardizing import statements and formatting in App.tsx, main.tsx, get-data.ts, and chart-utils.ts. Ensure proper return statement in useGetData hook.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
-import ChartView from './components/chart/chart-view'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'; 
+import ChartView from "./components/chart/chart-view";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
 
 function App() {
-
-  return (
-    <QueryClientProvider client={queryClient}>
-      <ChartView />
-    </QueryClientProvider>
-  )
-
+    return (
+        <QueryClientProvider client={queryClient}>
+            <ChartView />
+        </QueryClientProvider>
+    );
 }
 
-export default App
+export default App;
